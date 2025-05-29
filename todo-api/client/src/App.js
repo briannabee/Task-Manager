1819ebeb69c329722b409f709afa49feef3e4137
BRIANNA DYSINGER <BDYSINGER1@UCO.EDU>
2025-05-28 19:50:28 -0500

    stylization

--- todo-api/client/src/App.js
+++ todo-api/client/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState('');
 
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/tasks')
+      .then(res => setTasks(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  const addTask = () => {
+    axios.post('http://localhost:5000/api/tasks', { title })
+      .then(res => setTasks([...tasks, res.data]))
+      .catch(err => console.log(err));
+  };
+
+  return (
+  <div className="container">
+    <h1>📝 My Task Manager</h1>
+
+    <div className="task-input">
+      <input
+        placeholder="New task..."
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+    </div>
+
+    <ul>
+      {tasks.map(task => (
+        <li key={task.id}>{task.title}</li>
+      ))}
+    </ul>
+  </div>
+);
+}
 export default App;
